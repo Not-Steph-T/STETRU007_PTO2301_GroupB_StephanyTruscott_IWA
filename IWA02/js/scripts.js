99ae61f2d4c957e7a7753e0e47e7f5961ab5aa60
Stephany Truscott <st.truscott@gmail.com>
2023-04-23 12:12:22 +0200

    Completed IWA03 Challenges 1 & 2

---
+++ IWA02/js/scripts.js
@@ -0,0 +1,27 @@
+/* Challenge 1 */
+
+const user = 'John' // This is the firstname of the user
+const surname = 'Smith'  // This is the lastname of the user
+
+console.log(user,surname);
+
+/* Challenge 2 */
+
+const date = '10/07/2014' //This is the date that a user created their account 
+
+console.log(date)
+
+/* Challenge 3 */
+
+/* 
+It is important to show the following message in order to:
+- assure users
+- scare hackers
+- impress investors
+*/
+
+console.warn('Security scan starting');
+
+/* It is important to let user know when they can close the page  */
+
+console.info('Please wait for scan to complete before closing the browser'); 
