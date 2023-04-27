f969aaf244e1b1c90be20b1967919b90f27238ac
Stephany Truscott <st.truscott@gmail.com>
2023-04-27 09:57:06 +0200

    Updated IWA03 Challenge 2

--- IWA03/Challenge_2/js/johannes.js
+++ IWA03/Challenge_2/js/johannes.js
@@ -1,10 +1,9 @@
 // johannes.js
 
-{
-    const firstname = "Johannes"
-const surname = "Potgieter"
-const role = "Intern"
+
+const firstname = "Johannes";
+const surname = "Potgieter";
+export const role = "Intern";
 const display= firstname + " " + surname + " (" + role + ")"
 
 document.querySelector('#johannes').innerText = display
-}
---
+++ IWA04/js/script.js
@@ -0,0 +1,13 @@
+const date = '2050'
+const status = 'student'
+const count = '0' 
+
+if (date == 2050) {
+	console.log('January', 'New Years Day');
+	console.log('March', 'Human Rights Day');
+	console.log('April', 'Family Day');
+	console.log('May', 'Freedom Day');
+	count == count + '4'
+}
+
+console.log('The total holidays is:' + count)
