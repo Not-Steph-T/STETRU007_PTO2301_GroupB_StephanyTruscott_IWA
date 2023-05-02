1615488f43d81430e07a4e0dbae804395dd60e3e
Stephany Truscott <st.truscott@gmail.com>
2023-05-02 02:11:36 +0200

    Updated IWA04 Challenge

--- IWA04/js/script.js
+++ IWA04/js/script.js
@@ -1,27 +1,31 @@
-const DATE = '2050';
-const STATUS = 'parent';
-const COUNT = '0'; 
+var date = 2050;
+var state = ['student', 'parent'];
+var count = 0;
 
-console.log('January','New Years Day');
-console.log('March','Human Rights Day');
-date = 'April'
-console.log(date,'Family Day');
-console.log(date,'Freedom Day');
+console.log('January', 'New Years Day');
+console.log('March', 'Human Rights Day');
+date = 'April';
+console.log(date, 'Family Day');
+console.log(date, 'Freedom Day');
+count = count + 4;
 
-if (status = 'student') {
-	console.log('June', 'Youth Day'); 
+if (state === 'student') {
+    console.log('June', 'Youth Day');
+    count = count + 1;
 }
 
-console.log('August','Womens Day');
-console.log('September','Heritage Day');
-date = 'December'
-console.log(date,'Day of Reconciliation');
-console.log(date, 'Day of Goodwill')
+console.log('August', 'Womens Day');
+console.log('September', 'Heritage Day');
+date = 'December';
+console.log(date, 'Day of Reconciliation');
+console.log(date, 'Day of Goodwill');
+count = count + 4;
 
-if (status = 'parent') {
-	console.log(date, 'Christmas Day')
+if (state === 'parent') {
+    console.log(date, 'Christmas Day');
+    count = count + 1;
 }
 
-console.log('Your status is:', status);
-console.log('The year is:', date);
-console.log('The total holidays is:', COUNT + 10);
+console.log('Your status is :', state = 'student');
+console.log('The year is :', date);
+console.log('The total holidays is :', count);
