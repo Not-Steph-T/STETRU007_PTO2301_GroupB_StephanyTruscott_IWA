39683d51fb877c2fd0823f4915b7fd5824a77ea4
Stephany Truscott <st.truscott@gmail.com>
2023-06-11 23:23:19 +0200

    Completed IWA13 Challenge

---
+++ IWA13/js/scripts.js
@@ -0,0 +1,53 @@
+let state = "idle";
+let user = null;
+let calculated = "1";
+
+// calculated - string conversion & add calculated(for user & state)
+
+calculated == "string";
+let numberCalc = parseInt(calculated);
+let count = 0;
+
+const logCalc = () => {
+  numberCalc = calculated + 1;
+};
+
+// user & state - calcuser counter and define user & state variables
+
+const calcUser = () => {
+  calcUser.count = (calcUser.count || 0) + 1;
+
+  if (calcUser.count > 2) user == null;
+  user = "John";
+
+  if (calcUser.count > 2);
+  state = "requesting";
+
+  if (calcUser.count > 3);
+  state = "idle";
+};
+
+// final console log
+
+const checkUser = () => {
+  if (user == null && state === "Requesting") {
+  }
+  console.log(`User: ${user} (${calcUser.count})`);
+};
+
+// // Only allowed to change code above
+
+checkUser();
+calcUser();
+
+checkUser();
+calcUser();
+
+checkUser();
+calcUser();
+
+checkUser();
+calcUser();
+
+checkUser();
+calcUser();
