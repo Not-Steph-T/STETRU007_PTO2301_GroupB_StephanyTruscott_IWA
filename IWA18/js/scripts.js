2961eb45e44f93ea699e9bf0cf60696fc690b87c
Stephany Truscott <st.truscott@gmail.com>
2023-06-24 15:09:23 +0200

    Completed IWA18

---
+++ IWA18/js/scripts.js
@@ -0,0 +1,118 @@
+/**
+ * A handler that fires when a user drags over any element inside a column. In
+ * order to determine which column the user is dragging over the entire event
+ * bubble path is checked with `event.path` (or `event.composedPath()` for
+ * browsers that don't support `event.path`). The bubbling path is looped over
+ * until an element with a `data-area` attribute is found. Once found both the
+ * active dragging column is set in the `state` object in "data.js" and the HTML
+ * is updated to reflect the new column.
+ *
+ * @param {Event} event
+ */
+const handleDragOver = (event) => {
+  event.preventDefault();
+  const path = event.path || event.composedPath();
+  let column = null;
+
+  for (const element of path) {
+    const { area } = element.dataset;
+    if (area) {
+      column = area;
+      break;
+    }
+  }
+
+  if (!column) return;
+  updateDragging({ over: column });
+  updateDraggingHtml({ over: column });
+};
+
+const handleDragStart = (event) => {};
+
+const handleDragEnd = (event) => {};
+
+/**
+ * opens the "?" button and closes it when clicked on the "close" button
+ *
+ */
+const handleHelpToggle = (event) => {
+  const { target } = event;
+  if (target == html.other.help) {
+    html.help.overlay.showModal();
+  }
+  if (target == html.help.cancel) {
+    html.help.overlay.close();
+  }
+};
+
+/**
+ * when clicked on "Add Order" opens it.
+ * closes it when clicked on "cancel"
+ */
+const handleAddToggle = (event) => {
+  const { target } = event;
+  if (target == html.other.add) {
+    html.add.overlay.showModal();
+  }
+  if (target == html.add.cancel) {
+    html.add.overlay.close();
+  }
+};
+
+/**
+ * when user submits order, collects the info typed in and pushes it to the ordered column
+ *
+ */
+const handleAddSubmit = (event) => {
+  event.preventDefault();
+  const data = {
+    title: html.add.title.value,
+    table: html.add.table.value,
+    column: html.edit.column.value || html,
+  };
+  const props = createOrderData(data);
+  const content = createOrderHtml(props);
+  const orderedColumn = document.querySelector('[data-area="ordered"]');
+  const orderedDiv = orderedColumn.querySelector('[data-column="ordered"]');
+  orderedDiv.appendChild(content);
+  handleAddToggle();
+};
+
+/**
+ *
+ * alows you to click in data extracted and re edit it to be updated in the eirther column
+ */
+const handleEditToggle = (event) => {
+  const { target } = event;
+  const opened = document.querySelector(".order");
+  if (target == opened) {
+    html.edit.overlay.showModal();
+  }
+  if (target == html.edit.cancel) {
+    html.edit.overlay.close();
+  }
+};
+
+const handleEditSubmit = (event) => {};
+const handleDelete = (event) => {};
+
+html.add.cancel.addEventListener("click", handleAddToggle);
+html.other.add.addEventListener("click", handleAddToggle);
+html.add.form.addEventListener("submit", handleAddSubmit);
+
+html.other.grid.addEventListener("click", handleEditToggle);
+html.edit.cancel.addEventListener("click", handleEditToggle);
+html.edit.form.addEventListener("submit", handleEditSubmit);
+html.edit.delete.addEventListener("click", handleDelete);
+
+html.help.cancel.addEventListener("click", handleHelpToggle);
+html.other.help.addEventListener("click", handleHelpToggle);
+
+for (const htmlColumn of Object.values(html.columns)) {
+  htmlColumn.addEventListener("dragstart", handleDragStart);
+  htmlColumn.addEventListener("dragend", handleDragEnd);
+}
+
+for (const htmlArea of Object.values(html.area)) {
+  htmlArea.addEventListener("dragover", handleDragOver);
+}
